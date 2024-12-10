016d18358e1c1501b36a8c6f2793b66a0fdb4c6b
Ankita Chandrashekhar Buradkar <ankita.10825181@ltimindtree.com>
2024-12-10 11:55:07 +0530

    homepage changes done

--- loan-app/src/Components/FeatureCard.jsx
+++ loan-app/src/Components/FeatureCard.jsx
@@ -12,29 +12,20 @@ const FeatureCard = ({ img, title, description, btnText, link }) => {
   return (
     <div className="container card feature-card">
       <div class="card">
-
-<div class="slide slide1">
-
-    <div class="content">
-
+      <div class="slide slide1">
+      <div class="content">
         <div class="icon">
-
         <img
         src={img}
         className="card-img-top card-img"
         alt={title}
       />
-
         </div>
-
     </div>
-
 </div>
 
 <div class="slide slide2">
-
     <div class="content">
-
     <div className="card-body d-flex flex-column">
         <h5 className="card-title">{title}</h5>
         <p className="card-text">{description}</p>
@@ -50,23 +41,6 @@ const FeatureCard = ({ img, title, description, btnText, link }) => {
     </div>
 </div>
 </div>
-      {/* <img
-        src={img}
-        className="card-img-top card-img"
-        alt={title}
-      />
-      <div className="card-body d-flex flex-column">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{description}</p>
-        {btnText && (
-          <button
-            className="btn btn-danger mt-auto feature-card-button"
-            onClick={() => handleClick(link)}
-          >
-            {btnText}
-          </button>
-        )}
-      </div> */}
     </div>
     
   );
